65db8a1b6ad8eb59b46698965b7eb3d3ba821839
Vytor Rennan <crosdeadbr@gmail.com>
2023-06-24 18:37:04 -0300

    Animação do carrossel

--- static/js/projetos.js
+++ static/js/projetos.js
@@ -1,3 +1,4 @@
+// Cores
 document.getElementById('projetos').style.backgroundColor = '#147072'
 document.getElementById('projetos').style.color = '#fff'
 document.getElementById('sidebar-lower').style.borderColor = '#147072'
@@ -5,11 +6,13 @@ document.getElementsByClassName('container')[0].style.backgroundColor = 'rgb(20,
 document.querySelector(":root").style.setProperty("--footBorderColor", "#147072")
 
 
+// Carrossel
 let carrosselArray = [["../static/images/projetos/carrosel/podcastBanner.png", "Podcast", "https://www.google.com"], ["../static/images/projetos/carrosel/exterminandoDrogasBanner.png", "Exterminando Drogas", "https://www.youtube.com"]];
 let carrossel = document.querySelector(".carrossel");
 let carrosselLink = carrossel.childNodes[1];
 let carrosselImage = carrossel.childNodes[1].childNodes[1];
 
+let tempoDaAnimacao = parseFloat(getComputedStyle(carrosselImage).getPropertyValue("--transitionTime").replace("ms", ""));
 
 let countIndexCarrosel = 0;
 carrosselLink.href = carrosselArray[countIndexCarrosel][2];
@@ -22,11 +25,17 @@ carrosselArray.forEach((element, index) => {
     newBtnCarrossel.innerText = (index + 1).toString();
 
     newBtnCarrossel.addEventListener("click", () => {
-        carrosselLink.href = carrosselArray[index][2];
-        carrosselImage.alt = carrosselArray[index][1];
-        carrosselImage.src = carrosselArray[index][0];
-        countIndexCarrosel = index;
-        destacarBtn(countIndexCarrosel);
+        carrosselImage.style.height = "0%";
+        
+        setTimeout(() => {
+            carrosselLink.href = carrosselArray[index][2];
+            carrosselImage.alt = carrosselArray[index][1];
+            carrosselImage.src = carrosselArray[index][0];
+            countIndexCarrosel = index;
+            destacarBtn(countIndexCarrosel);
+            carrosselImage.style.height = "100%";
+        }, tempoDaAnimacao)
+        
     })
 
     carrossel.childNodes[3].appendChild(newBtnCarrossel);
@@ -41,10 +50,14 @@ setInterval(() => {
     if (countIndexCarrosel > (carrosselArray.length - 1)) {
         countIndexCarrosel = 0;
     }
-    carrosselLink.href = carrosselArray[countIndexCarrosel][2];
-    carrosselImage.alt = carrosselArray[countIndexCarrosel][1];
-    carrosselImage.src = carrosselArray[countIndexCarrosel][0];
-    destacarBtn(countIndexCarrosel);
+    carrosselImage.style.height = "0%";
+    setTimeout(() => {
+        carrosselLink.href = carrosselArray[countIndexCarrosel][2];
+        carrosselImage.alt = carrosselArray[countIndexCarrosel][1];
+        carrosselImage.src = carrosselArray[countIndexCarrosel][0];
+        destacarBtn(countIndexCarrosel);
+        carrosselImage.style.height = "100%";
+    }, tempoDaAnimacao)
 }, 5000)
 
 
